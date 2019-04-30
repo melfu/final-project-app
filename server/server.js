aa969aedae4b17807a72fa64fe2d26269f0938cb
melfu <melular@gmail.com>
2019-04-30 01:35:01 -0500

    df

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static('public'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'final-project', 'public', 'index.html'))
   })
 }
 
