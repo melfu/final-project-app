3406e899ab22a73539a883628010396be1a592c5
melfu <melular@gmail.com>
2019-04-30 22:32:43 -0500

    build folder

---
+++ public/precache-manifest.f572474f3bacc27871ff55f9793e9f54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a3b25c95eee2de3f4f1a",
+    "url": "/static/js/main.9f8770b4.chunk.js"
+  },
+  {
+    "revision": "b68596ad550acdec1a7e",
+    "url": "/static/js/2.fd1ffca7.chunk.js"
+  },
+  {
+    "revision": "a3b25c95eee2de3f4f1a",
+    "url": "/static/css/main.2e0e3a92.chunk.css"
+  },
+  {
+    "revision": "88c287d83f847e6d7cf597e4380cd915",
+    "url": "/index.html"
+  }
+];
