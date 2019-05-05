d814aaa15f50e78baf7851ba9e1290b0532721fe
melfu <melular@gmail.com>
2019-05-05 15:18:51 -0500

    fixed server.js app.get path

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static('public'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   })
 }
 
