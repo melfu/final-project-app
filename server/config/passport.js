cf0315c157e4d7f96c0c1f73ae2b667a8065a20b
melfu <melular@gmail.com>
2019-05-05 15:07:59 -0500

    fixed keys in passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,10 +2,11 @@ const JwtStrategy = require("../../node_modules/passport-jwt/lib").Strategy;
 const ExtractJwt = require("../../node_modules/passport-jwt/lib").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const keys = require("./keys");
+//const keys = require("./keys");
 const opts = {};
+const secretOrKey = process.env.secretOrKey;
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secretOrKey;
+opts.secretOrKey = secretOrKey;
 module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
