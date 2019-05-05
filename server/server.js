fbd367863c1d2b4e38476e02c34ea89576f2590d
melfu <melular@gmail.com>
2019-05-05 16:38:22 -0500

    changes for heroku

--- server/server.js
+++ server/server.js
@@ -1,20 +1,19 @@
-
-console.log('final-project/server/server.js');
-require('dotenv').config();
-const express = require('../node_modules/express');
+console.log("final-project/server/server.js");
+require("dotenv").config();
+const express = require("../node_modules/express");
 // const cors = require("cors");
-const bodyParser = require('../node_modules/body-parser');
+const bodyParser = require("../node_modules/body-parser");
 // const user = require('./routes/user.route'); // Imports routes for the products
-const authuser = require('../src/routes/api/users'); // Imports routes for the products
+const authuser = require("../src/routes/api/users"); // Imports routes for the products
 const passport = require("../node_modules/passport/lib");
 const users = require("../src/routes/api/users");
-const path = require('path');
-var cors = require('../node_modules/cors/lib');
+const path = require("path");
+var cors = require("../node_modules/cors/lib");
 
 const app = express();
 // const router = express.Router();
 // this is our MongoDB database
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // DB Config
 // const dbRoute = require("./config/keys").mongoURI;
 app.use(cors());
@@ -26,6 +25,7 @@ app.use(
 );
 app.use(bodyParser.json());
 
+console.log(path.resolve(__dirname, "..", "build", "index.html"));
 //connects our back end code with the database
 // mongoose.connect(
 //   dbRoute,
@@ -37,7 +37,10 @@ mongoose.connect(mongoURI);
 mongoose.Promise = global.Promise;
 
 let dbMongoose = mongoose.connection;
-dbMongoose.on('error', console.error.bind(console, 'MongoDB connection error:'));
+dbMongoose.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
 
 // Passport middleware
 app.use(passport.initialize());
@@ -51,24 +54,24 @@ app.use("/api/users", users);
 // app.use(cors());
 
 // app.use('/user', user);
-app.use('/api', authuser);
+app.use("/api", authuser);
 
 // app.use(app.router);
 // routes.initialize(app);
 
 // Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
-  // Set static folder 
-  app.use(express.static('public'))
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  //app.use(express.static("public"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../build/index.html"));
+  });
 }
 
 //Next step would be dedicating a port number and telling our express app to listen to that port.
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
 app.listen(port, () => {
-    console.log('Server is up and running on port number ' + port);
+  console.log("Server is up and running on port number " + port);
 });
